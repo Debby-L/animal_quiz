a85b537ca2ec626b1249fcd46b0ac213c710e899
Debby Lai <debbylai1229@gmail.com>
2021-02-20 11:53:05 +0800

    增加答題難度減少答題次數, 加上多選題和是非題選項

--- animal_quiz.py
+++ animal_quiz.py
@@ -1,20 +1,19 @@
 def check_guess(guess, answer):
 	global score
-	#改成玩家三次答題機會
 	still_guessing = True
 	attempt = 0
-	while still_guessing and attempt < 3:
-	 # 檢查是否玩過三次或玩家已經猜到正確答案,只要符合其中一項條件就不會再執行迴圈
+	while still_guessing and attempt < 2:
+	#增加答題難度，減少答題次數
 	    if guess.lower() == answer.lower(): #將玩家與正確答案全部都轉成小寫
 	       print('Correct answer')
-	       score = score + 3 - attempt #三次機會(滿分)扣掉答錯次數
+	       score = score + 2 - attempt #2次機會(滿分)扣掉答錯次數
 	       still_guessing = False
 	    else:
-	    	if attempt < 2:
+	    	if attempt < 1:
 	    		guess = input('Sorry wrong answer. Try again.')
 	    	attempt = attempt + 1 #玩家用掉的答題次+1
-	    	if attempt == 3:
-	    		print('The correct answer is' + answer)
+	    	if attempt == 2:
+	    		print('The correct answer is' +  answer)
 score = 0
 print('Guess the Animals!')
 guess1 = input('Which bear lives at the North Pole?')
@@ -23,6 +22,8 @@ guess2 = input('Which is the fastest land animal?')
 check_guess(guess2, 'chetah')
 guess3 = input('Which is the largest animal?')
 check_guess(guess3, 'blue whale')
-
-
-print('Your score is' +  str(score))
+guess4 = input('Which one of theseis a fish?\n A) whale\n B) dolphin\n C) shark\n D) squid\n Type A, B, C or D ')
+check_guess(guess4, 'C') #選擇題, \n=換行
+guess5 = input('Mice are animals. True or False?')
+check_guess(guess5, 'True')
+print('Your score is' +   str(score))
