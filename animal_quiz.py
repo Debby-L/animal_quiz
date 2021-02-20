dd814f7b924a3c65cd1485ce7d35f3cfc77c6cf8
Debby Lai <debbylai1229@gmail.com>
2021-02-20 11:37:10 +0800

    改變分數計算

--- animal_quiz.py
+++ animal_quiz.py
@@ -7,14 +7,14 @@ def check_guess(guess, answer):
 	 # 檢查是否玩過三次或玩家已經猜到正確答案,只要符合其中一項條件就不會再執行迴圈
 	    if guess.lower() == answer.lower(): #將玩家與正確答案全部都轉成小寫
 	       print('Correct answer')
-	       score = score + 1
+	       score = score + 3 - attempt #三次機會(滿分)扣掉答錯次數
 	       still_guessing = False
 	    else:
 	    	if attempt < 2:
 	    		guess = input('Sorry wrong answer. Try again.')
 	    	attempt = attempt + 1 #玩家用掉的答題次+1
 	    	if attempt == 3:
-	    		print('The correct answer is' +  answer)
+	    		print('The correct answer is' + answer)
 score = 0
 print('Guess the Animals!')
 guess1 = input('Which bear lives at the North Pole?')
@@ -23,4 +23,6 @@ guess2 = input('Which is the fastest land animal?')
 check_guess(guess2, 'chetah')
 guess3 = input('Which is the largest animal?')
 check_guess(guess3, 'blue whale')
+
+
 print('Your score is' +  str(score))
